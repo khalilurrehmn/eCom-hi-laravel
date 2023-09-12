c780c4e55989fdf76ff93489a0b3486e35fd7f3d
khalilurrehmn <khalil939244@gmail.com>
2023-09-12 12:53:25 +0500

    login and logout done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -20,9 +20,36 @@
 
 <style>
 .custom-login{
-    padding-top: 100px;
     height: 500px;
+    padding-top: 100px;
 
+
+}
+img.slider-img{
+    height: 400px !important;
+}
+.custom-product{
+    height: 600px;
+}
+.slider-text{
+    background-color: #24465454 !important
+}
+.trending-img{
+    height: 100px;
+}
+.trending-items{
+
+    float: left;
+    width: 20%;
+}
+.trending-wrapper{
+    margin: 20px;
+}
+.detail-img{
+    height: 200px;
+}
+.search-box{
+    width: 500px !important;
 }
 </style>
 
