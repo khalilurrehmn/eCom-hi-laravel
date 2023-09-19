5e22291152e1d95e63d90d94a1996495a23d5735
khalilurrehmn <khalil939244@gmail.com>
2023-09-19 12:04:47 +0500

    all done

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,10 @@ Route::get('/logout', function () {
     Session::forget('user');
     return redirect('/login');
 });
-
+Route::view('/register','register');
 Route::view('/login','login');
 Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
 Route::get('/', [ProductController::class, 'index']);
 Route::get('detail/{id}', [ProductController::class, 'detail']);
 Route::get('search', [ProductController::class, 'search']);
